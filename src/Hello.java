adc0ae72d7d13832a880f1292ae66085015eeba8
Carter <Carter@DESKTOP-QTPCRDO.unl.edu>
2025-09-01 11:45:17 -0500

    Pushed from copied repo

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Carter Crist");
 		System.out.println("Computer Science");
+		
+		System.out.println("Carter Crist");
+		System.out.print("CS");
 	}
 
 }
