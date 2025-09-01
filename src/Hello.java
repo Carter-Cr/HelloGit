46579f649275c9f8b78c862e7492990d109628f2
Carter <Carter@DESKTOP-QTPCRDO.unl.edu>
2025-09-01 11:48:38 -0500

    Print

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		System.out.println("Carter Crist");
 		System.out.println("Computer Science");
 		
+		System.out.println("copied my own repo and pulled from there");
 		System.out.println("Carter Crist");
 		System.out.print("CS");
 	}
